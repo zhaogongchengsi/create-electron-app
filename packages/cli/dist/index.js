57bd27c145a62cfb86a5761f274696768ff49a02
zhaogongchengsi <zzh1586169624@163.com>
2022-08-29 13:53:43 +0800

    init 2

---
+++ packages/cli/dist/index.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+
+function build() {
+  console.log("build");
+}
+
+export { build };
--- packages/cli/rollup.config.js
+++ packages/cli/rollup.config.js
@@ -1,4 +1,33 @@
-/**
- * @type { rollup }
- */
-export default {};
+import { join, resolve } from "path";
+import esbuild from "rollup-plugin-esbuild";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+
+const resolvePath = (path) => resolve(join(process.cwd(), path));
+
+export default {
+  input: resolvePath("./src/index.ts"),
+  output: {
+    file: "./dist/index.js",
+    format: "es",
+    intro: "#!/usr/bin/env node",
+  },
+  plugins: [
+    nodeResolve(),
+    commonjs(),
+    esbuild({
+      // All options are optional
+      include: /\.[jt]sx?$/, // default, inferred from `loaders` option
+      exclude: /node_modules/, // default
+      sourceMap: true, // default
+      minify: process.env.NODE_ENV === "production",
+      target: "esnext", // default, or 'es20XX', 'esnext'
+      tsconfig: "tsconfig.json", // default
+      loaders: {
+        ".json": "json",
+        ".ts": "ts",
+        ".js": "js",
+      },
+    }),
+  ],
+};
