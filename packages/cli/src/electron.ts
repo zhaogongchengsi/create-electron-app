81aa3347dc73287fad404b80d5d611375797a3c1
zhaogongchengsi <zzh1586169624@163.com>
2022-08-31 11:16:26 +0800

    新增进程关闭

--- packages/cli/src/electron.ts
+++ packages/cli/src/electron.ts
@@ -1,4 +1,4 @@
-import { exec, spawn } from "node:child_process";
+import { spawn } from "node:child_process";
 
 export async function createDevElectronApp(root: string, file: string) {
   const ls = spawn(`electron`, [file], {
@@ -7,7 +7,7 @@ export async function createDevElectronApp(root: string, file: string) {
   });
 
   ls.on("close", (code) => {
-    console.log(`child process exited with code ${code}`);
+    process.exit(1);
   });
 
   return ls;
