79f8feec37febbe7e3ea30256453a773595d42d1
zhaogongchengsi <zzh1586169624@163.com>
2022-09-01 17:58:50 +0800

    优化打包逻辑

--- packages/cli/rollup.config.js
+++ packages/cli/rollup.config.js
@@ -1,5 +1,5 @@
 import { join, resolve } from "path";
-// import esbuild from "rollup-plugin-esbuild";
+import esbuild from "rollup-plugin-esbuild";
 import typescript from "@rollup/plugin-typescript";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
@@ -7,31 +7,33 @@ import json from "@rollup/plugin-json";
 
 const resolvePath = (path) => resolve(join(process.cwd(), path));
 
+const selectTsCompiler = () => {
+  const { NODE_ENV } = process.env;
+  return NODE_ENV === "production"
+    ? typescript({
+        declaration: true,
+        declarationDir: "dist/types",
+        sourceMap: true,
+      })
+    : esbuild({
+        // All options are optional
+        include: /\.[jt]sx?$/, // default, inferred from `loaders` option
+        exclude: /node_modules/, // default
+        sourceMap: true, // default
+        minify: process.env.NODE_ENV === "production",
+        target: "esnext", // default, or 'es20XX', 'esnext'
+        tsconfig: "tsconfig.json", // default
+        loaders: {
+          ".json": "json",
+          ".ts": "ts",
+          ".js": "js",
+        },
+      });
+};
+
 export default async function () {
-  const plugins = [
-    nodeResolve(),
-    commonjs(),
-    json(),
-    typescript({
-      declaration: true,
-      declarationDir: "dist/types",
-      sourceMap: true,
-    }),
-    // esbuild({
-    //   // All options are optional
-    //   include: /\.[jt]sx?$/, // default, inferred from `loaders` option
-    //   exclude: /node_modules/, // default
-    //   sourceMap: true, // default
-    //   minify: process.env.NODE_ENV === "production",
-    //   target: "esnext", // default, or 'es20XX', 'esnext'
-    //   tsconfig: "tsconfig.json", // default
-    //   loaders: {
-    //     ".json": "json",
-    //     ".ts": "ts",
-    //     ".js": "js",
-    //   },
-    // }),
-  ];
+  const tsPlugin = selectTsCompiler();
+  const plugins = [nodeResolve(), commonjs(), json(), tsPlugin];
 
   const indexConfig = {
     input: resolvePath("./src/index.ts"),
