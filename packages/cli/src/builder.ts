7fe4af29f15e1b44f51ebe5fb5ba542bf6cf8a4b
zhaogongchengsi <zzh1586169624@163.com>
2023-09-02 17:14:22 +0800

    fix: 修复 打包时找不到 主文件的bug

--- packages/cli/src/builder.ts
+++ packages/cli/src/builder.ts
@@ -2,10 +2,12 @@ import { loadElectronBuilder } from './load'
 import type { UltimatelyCeaConfig } from './config'
 
 export async function builder(config: UltimatelyCeaConfig) {
+  const { root } = config
   const { build, Platform } = loadElectronBuilder(config)
 
   const res = await build({
     targets: Platform.WINDOWS.createTarget(),
+    projectDir: root,
     config: config.build,
   })
 
