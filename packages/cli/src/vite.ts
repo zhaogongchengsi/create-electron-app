b8c97d502ebfa55b4af712a34d93e56467188f5a
zhaogongchengsi <zzh1586169624@163.com>
2023-09-06 13:57:09 +0800

    todo: 添加todo

--- packages/cli/src/vite.ts
+++ packages/cli/src/vite.ts
@@ -7,21 +7,16 @@ export function getInputOutDir(config: ResolvedConfig) {
   const libOptions = options.lib
 
   const resolve = (p: string) => _resolve(config.root, p)
-  const input = libOptions
-	  ? options.rollupOptions?.input
-		|| (typeof libOptions.entry === 'string'
-		  ? resolve(libOptions.entry)
-		  : Array.isArray(libOptions.entry)
-		  ? libOptions.entry.map(resolve)
-		  : Object.fromEntries(
-			  Object.entries(libOptions.entry).map(([alias, file]) => [
-		        alias,
-		        resolve(file),
-			  ]),
-		    ))
-	  : typeof options.ssr === 'string'
-	  ? resolve(options.ssr)
-	  : options.rollupOptions?.input || resolve('index.html')
 
-	  const outDir = resolve(options.outDir)
+  const input = libOptions
+    ? options.rollupOptions?.input || (typeof libOptions.entry === 'string'
+      ? resolve(libOptions.entry)
+      : Array.isArray(libOptions.entry)
+        ? libOptions.entry.map(resolve)
+        : Object.fromEntries(Object.entries(libOptions.entry).map(([alias, file]) => [alias, resolve(file)])))
+    : typeof options.ssr === 'string'
+      ? resolve(options.ssr)
+      : options.rollupOptions?.input || resolve('index.html')
+
+  const outDir = resolve(options.outDir)
 }
